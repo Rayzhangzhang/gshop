a9bebeda48ed847690952e07dbec9d705d6c5a96
Rayzhangzhang <252244566@qq.com>
2019-07-04 13:34:03 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter" />
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+    FooterGuide
+  }
+}
+
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    color:red
+</style>
